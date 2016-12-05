ca2f88c9a425925ba8893cc2630a594470fb384c
Isaac <isaac@web113064.wireless.mcgill.ca>
2016-12-04 19:26:11 -0500

    Fix attempt 2

--- src/JavaCool303/Cool303Container.java
+++ src/JavaCool303/Cool303Container.java
@@ -1,41 +1,41 @@
-//package JavaCool303;
-//
-//import java.util.*;
-//import javax.swing.*;
-//import java.awt.*;
-//
-//public class Cool303Container extends Cool303Component {
-//
-//public Cool303Container(String aLabel, Cool303Theme aTheme) {
-//  this.setTheme = aTheme;
-//  label = aLabel;
-//
-//  componentList = new ArrayList();
-//  componentCount = 0;
-//  mimimumComponentArea = 0;
-//
-//}
-//
-//void addComponent(Cool303Component aComponent) {
-//
-//  componentList.add(aComponent);
-//  componentCount++;
-//  minimumComponentArea = minimumComponentArea + aComponent.getArea();
-//
-//}
-//
-//public int getArea() {
-//
-//  return minimumComponentArea;
-//}
-//
-//private void editLabel(string newLabel) {
-//  label = newLabel;
-//}
-//
-//private string label;
-//private ArrayList<Cool303Component> componentList;
-//private int componentCount;
-//private int minimumComponentArea;
-//private int containerArea;
-//}
+package JavaCool303;
+
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class Cool303Container extends Cool303Component {
+
+public Cool303Container(String aLabel, Cool303Theme aTheme) {
+  this.setTheme = aTheme;
+  label = aLabel;
+
+  componentList = new ArrayList();
+  componentCount = 0;
+  mimimumComponentArea = 0;
+
+}
+
+void addComponent(Cool303Component aComponent) {
+
+  componentList.add(aComponent);
+  componentCount++;
+  minimumComponentArea = minimumComponentArea + aComponent.getArea();
+
+}
+
+public int getArea() {
+
+  return minimumComponentArea;
+}
+
+private void editLabel(string newLabel) {
+  label = newLabel;
+}
+
+private string label;
+private ArrayList<Cool303Component> componentList;
+private int componentCount;
+private int minimumComponentArea;
+private int containerArea;
+}
