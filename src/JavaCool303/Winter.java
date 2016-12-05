c6ce761766c33ea35ba2b69cf90abf50c541bfba
Alec Harmon <alecblumenfeld@gmail.com>
2016-12-04 19:26:36 -0500

    added winter

---
+++ src/JavaCool303/Winter.java
@@ -0,0 +1,29 @@
+package JavaCool303;
+import java.awt.Color;
+
+/**
+ * Created by alec on 12/4/16.
+ */
+
+public class Winter implements Cool303Theme {
+    private Color fontColor = Color.WHITE;
+    private String fontName = "Comic Sans MS";
+    private Color backgroundColor = Color.BLUE;
+    private Color buttonColor = Color.GRAY;
+
+    public Color getFontColor() {
+        return fontColor;
+    }
+
+    public String getFontName() {
+        return fontName;
+    }
+
+    public Color getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public Color getButtonStyle() {
+        return buttonColor;
+    }
+}
